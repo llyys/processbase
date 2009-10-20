1325d52e0c56ab42e04a8c5e12fb6cb4dfda6654
marat.gubaidullin <marat.gubaidullin@540fb962-8f43-11de-a8bd-9d4f4accca5f>
2009-10-20 19:05:09 +0000

    Initial Code
    git-svn-id: http://processbase.googlecode.com/svn/trunk@30 540fb962-8f43-11de-a8bd-9d4f4accca5f

---
+++ ProcessBaseHook/src/org/processbase/hook/EmptyHook.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C) 2009  Marat Gubaidullin.
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ **/
+package org.processbase.hook;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.ow2.bonita.definition.TxHook;
+import org.ow2.bonita.facade.APIAccessor;
+import org.ow2.bonita.facade.runtime.ActivityBody;
+import org.ow2.bonita.facade.runtime.ActivityInstance;
+
+/**
+ *
+ * @author mgubaidullin
+ */
+public class EmptyHook implements TxHook {
+
+    public void execute(APIAccessor apiAccessor, ActivityInstance<ActivityBody> activity) throws Exception {
+        Logger.getLogger(EmptyHook.class.getName()).log(Level.SEVERE, "EmptyHook executed " + activity.getUUID().toString());
+    }
+}
