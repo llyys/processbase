37058216a4e687e48d04f8b8e281d9bd3b23a566
marat.gubaidullin <marat.gubaidullin@540fb962-8f43-11de-a8bd-9d4f4accca5f>
2009-11-19 08:08:25 +0000

    initial
    git-svn-id: http://processbase.googlecode.com/svn/trunk@49 540fb962-8f43-11de-a8bd-9d4f4accca5f

---
+++ ProcessBaseServer1/src/org/processbase/acl/ProcessBaseRoleMapper.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (C) 2009  Marat Gubaidullin.
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ **/
+package org.processbase.acl;
+
+import java.io.File;
+import java.io.FileInputStream;
+import javax.naming.NamingException;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Properties;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import org.ow2.bonita.definition.RoleMapper;
+import org.ow2.bonita.facade.QueryAPIAccessor;
+import org.ow2.bonita.facade.uuid.ProcessInstanceUUID;
+
+/**
+ *
+ * @author mgubaidullin
+ */
+public class ProcessBaseRoleMapper implements RoleMapper {
+
+    public static Properties properties = new Properties();
+    public Hashtable env = null;
+    public DirContext ctx = null;
+
+    public Set<String> searchMembers(QueryAPIAccessor queryAPIAccessor, ProcessInstanceUUID piUUID, String rolename) throws Exception {
+        HashSet<String> result = new HashSet<String>();
+        return result;
+    }
+
+    public void loadProperties() {
+        try {
+            File file = new File("processbase1.properties");
+            FileInputStream fis = new FileInputStream(file);
+            properties.loadFromXML(fis);
+            fis.close();
+        } catch (Exception ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, ex.getMessage());
+        }
+    }
+
+    public void connect() throws NamingException {
+        env = new Hashtable();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        env.put(Context.PROVIDER_URL, "ldap://" + properties.getProperty("LDAP_HOST") + ":" + properties.getProperty("LDAP_PORT") + "/");
+        env.put(Context.SECURITY_AUTHENTICATION, "simple");
+        env.put(Context.SECURITY_PRINCIPAL, properties.getProperty("LDAP_ADMIN_USERNAME"));
+        env.put(Context.SECURITY_CREDENTIALS, properties.getProperty("LDAP_ADMIN_PASSWORD"));
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        ctx = new InitialDirContext(this.env);
+    }
+}
---
+++ ProcessBaseServer1/src/org/processbase/hook/EmptyHook.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) 2009  Marat Gubaidullin.
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ **/
+package org.processbase.hook;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.ow2.bonita.definition.Hook;
+import org.ow2.bonita.facade.QueryAPIAccessor;
+import org.ow2.bonita.facade.runtime.ActivityBody;
+import org.ow2.bonita.facade.runtime.ActivityInstance;
+
+/**
+ *
+ * @author mgubaidullin
+ */
+public class EmptyHook implements Hook {
+
+    public void execute(QueryAPIAccessor queryAPIAccessor, ActivityInstance<ActivityBody> activity) throws Exception {
+        Logger.getLogger(EmptyHook.class.getName()).log(Level.SEVERE, "EmptyHook executed " + activity.getUUID().toString());
+
+    }
+}
