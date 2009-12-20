5d7bb5150074bfded2a230dacaee98e8848b8ff5
marat.gubaidullin <marat.gubaidullin@540fb962-8f43-11de-a8bd-9d4f4accca5f>
2009-12-20 19:17:14 +0000

    09.12.20
    git-svn-id: http://processbase.googlecode.com/svn/trunk@75 540fb962-8f43-11de-a8bd-9d4f4accca5f

--- ProcessBaseServer1/src/org/processbase/hook/ProcessBaseTxHook.java
+++ ProcessBaseServer1/src/org/processbase/hook/ProcessBaseTxHook.java
@@ -3,6 +3,8 @@ package org.processbase.hook;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.ow2.bonita.definition.TxHook;
 import org.ow2.bonita.facade.APIAccessor;
 import org.ow2.bonita.facade.def.majorElement.DataFieldDefinition;
@@ -10,6 +12,7 @@ import org.ow2.bonita.facade.exception.ProcessNotFoundException;
 import org.ow2.bonita.facade.runtime.ActivityBody;
 import org.ow2.bonita.facade.runtime.ActivityInstance;
 import org.processbase.bpm.BPMModule;
+import org.processbase.util.Constants;
 
 /**
  *
@@ -21,14 +24,19 @@ public class ProcessBaseTxHook implements TxHook {
     protected Map<String, Object> processVars = new HashMap<String, Object>();
     protected Set<DataFieldDefinition> dfds = null;
 
-    public void execute(ActivityInstance<ActivityBody> activity) throws ProcessNotFoundException, Exception {
+    public void execute(ActivityInstance<ActivityBody> activity) throws Exception {
         
     }
 
     public void execute(APIAccessor apiAccessor, ActivityInstance<ActivityBody> activity) throws Exception {
-        bpmModule = new BPMModule("");
+        Logger.getLogger("DEBUG").log(Level.SEVERE, "1111111111111111111111111111111");
+        Constants.loadConstants();
+        bpmModule = new BPMModule("PROCESBASE");
+        Logger.getLogger("DEBUG").log(Level.SEVERE, "222222222222222222222222222222222222222222");
         dfds = bpmModule.getProcessDataFields(activity.getProcessDefinitionUUID());
+        Logger.getLogger("DEBUG").log(Level.SEVERE, "dfds = " + dfds.toString());
         processVars = bpmModule.getProcessInstanceVariables(activity.getProcessInstanceUUID(), true);
+        Logger.getLogger("DEBUG").log(Level.SEVERE, "pbVars = " + processVars.toString());
         execute(activity);
     }
 
